912f827e65072f4fb4416b414b69be9d90831a4b
NathalieB <nbonatout@gmail.com>
2013-12-07 12:07:28 +0100

    Script Matlab - Change Name File

--- Project5/MatlabPlot3D.m
+++ Project5/MatlabPlot3D.m
@@ -5,22 +5,24 @@ clear,clf
 % File to open:
 % Input Parameters
 nPosi = 2; % From 0 to nPosi
-typeSolver = 'rk4';
-rows = 1000000;% 300*365; % Number of time steps
-nbPlanets = 2; % Number of Planets
+nameFileToOpen = 'sim_0_';
+typeSolver = 'leapfrog';
+rows = 100;% 300*365; % Number of time steps
+nbPlanets = 100; % Number of Planets
 printingSteps = 10; % Printing every x time steps ... Don't use it now, but ... Just in case ?
-bWantAGif = false; % A Gif or a JPEG ?
+bWantAGif = true; % A Gif or a JPEG ?
 bWantToSave = false; % This considerably slows down everything, but well ... !
 % Then processing with the opening
-fileToOpen_X = strcat('sim_BM_','pos',int2str(nPosi-2),'_',typeSolver,'.dat');
-fileToOpen_Y = strcat('sim_BM_','pos',int2str(nPosi-1),'_',typeSolver,'.dat');
-fileToOpen_Z = strcat('sim_BM_','pos',int2str(nPosi),'_',typeSolver,'.dat');
+fileToOpen_X = strcat(nameFileToOpen,'pos',int2str(nPosi-2),'_',typeSolver,'.dat');
+fileToOpen_Y = strcat(nameFileToOpen,'pos',int2str(nPosi-1),'_',typeSolver,'.dat');
+fileToOpen_Z = strcat(nameFileToOpen,'pos',int2str(nPosi),'_',typeSolver,'.dat');
 fidPosi_X = fopen(fileToOpen_X );
 fidPosi_Y = fopen(fileToOpen_Y );
 fidPosi_Z = fopen(fileToOpen_Z );
 Posi_X = fscanf(fidPosi_X,'%g',[nbPlanets rows]).';
 Posi_Y = fscanf(fidPosi_Y,'%g',[nbPlanets rows]).';
 Posi_Z = fscanf(fidPosi_Z,'%g',[nbPlanets rows]).';
+
 
 % And finally closing the handle on the open documents
 fclose(fidPosi_X);
@@ -29,17 +31,19 @@ fclose(fidPosi_Z);
 
 if bWantAGif == true
     % Selection of the name of the file:
-    filename = strcat('plot3D_for_' ,typeSolver,'.gif');
+    filename = strcat('plot3D_for_',nameFileToOpen ,typeSolver,'.gif');
     figure(1)
     grid on;
     h = plot3(Posi_X(:,1),Posi_Y(:,1),Posi_Z(:,1),'r.');
-    xlim([-2.0e+11 2.0e+11]);
-    ylim([-2.0e-04    2.0e-04]);
-    zlim([-2.0e+12 2.0e+12]);
+    
     xlabel('X');
     ylabel('Y');
     zlabel('Z');
     title(['Plot with ', typeSolver]);
+   axis([-40 40 -40 40 -40 40]);
+   % xlim([-40 40]);
+    %ylim([-40 40]);
+    %zlim([-40 40])
     for i = 2: rows
         %if bWantToSave == true
                 set(h,'XData',Posi_X(i,:),'YData',Posi_Y(i,:),'ZData',Posi_Z(i,:));
@@ -58,23 +62,24 @@ if bWantAGif == true
 %             grid on;
 %             drawnow
 %         end
-        pause(0.1);
+       % pause(0.1);
     end
 else
     figure(1)
     
-    filename = strcat('plot3D_for_',typeSolver,'.jpeg');
+    filename = strcat('plot3D_for_',nameFileToOpen,typeSolver,'.jpeg');
     for i = 1: rows
        
             % Do we need a position picture for every planet ? Not really ~
             plot3(Posi_X(i,:),Posi_Y(i,:),Posi_Z(i,:),'r.');
-            grid on;
-            xlabel('X');
-            ylabel('Y');
-            zlabel('Z');
+
             hold on;
             drawnow
     end
+    grid on;
+    xlabel('X');
+    ylabel('Y');
+    zlabel('Z');
     title(['Plot with ', typeSolver]);
     frame = getframe(1);
     im = frame2im(frame);
